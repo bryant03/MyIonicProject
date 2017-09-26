e94a263b0626c50f750d32258e89ea9c18783c70
bryant03 <605317365@qq.com>
2017-09-26 16:44:37 +0800

    update first time

---
+++ src/app/app.module.ts
@@ -0,0 +1,47 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule, ErrorHandler } from '@angular/core';
+import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
+import { MyApp } from './app.component';
+
+import { HelloIonicPage } from '../pages/hello-ionic/hello-ionic';
+import { ItemDetailsPage } from '../pages/item-details/item-details';
+import { InputFloatingLabels } from '../pages/input-floating-labels/input-floating-labels';
+import { ListPage } from '../pages/list/list'; 
+import { Alert } from '../pages/alert/alert';
+import { OtherPage } from '../pages/other-page/other-page';
+
+
+import { StatusBar } from '@ionic-native/status-bar';
+import { SplashScreen } from '@ionic-native/splash-screen';
+
+@NgModule({
+  declarations: [
+    MyApp,
+    HelloIonicPage,
+    ItemDetailsPage,
+    ListPage,
+    Alert,
+    InputFloatingLabels,
+    OtherPage
+  ],
+  imports: [
+    BrowserModule,
+    IonicModule.forRoot(MyApp),
+  ],
+  bootstrap: [IonicApp],
+  entryComponents: [
+    MyApp,
+    HelloIonicPage,
+    ItemDetailsPage,
+    ListPage,
+    Alert,
+    InputFloatingLabels,
+    OtherPage
+  ],
+  providers: [
+    StatusBar,
+    SplashScreen,
+    {provide: ErrorHandler, useClass: IonicErrorHandler}
+  ]
+})
+export class AppModule {}
